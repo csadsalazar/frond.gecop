ea3be445f6a0315f4aae3461c813b278b5d321c7
Cesar David Salazar Torres <csalazart@invima.gov.co>
2024-05-24 17:03:23 -0500

    Modularizacion del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CollectionsComponent } from './collections/collections.component';
+import { LawyersComponent } from './lawyers/lawyers.component';
+import { AlertsComponent } from './alerts/alerts.component';
+import { TermsComponent } from './terms/terms.component';
+import { ControlpanelComponent } from './controlpanel/controlpanel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TopbarComponent,
+    TopbarComponent,
+    NavbarComponent,
+    CollectionsComponent,
+    LawyersComponent,
+    AlertsComponent,
+    TermsComponent,
+    ControlpanelComponent,
   ],
   imports: [
     BrowserModule,
@@ -15,4 +33,4 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { }                                
